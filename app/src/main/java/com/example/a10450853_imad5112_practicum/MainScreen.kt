f80019be09ef23ae4ee9fda35ad50047ca21197c
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2024-06-10 11:52:05 +0200

    Added methods to the code for adding the information and clearing it from out array. Added a third screen for our detailed view.

--- app/src/main/java/com/example/a10450853_imad5112_practicum/MainScreen.kt
+++ app/src/main/java/com/example/a10450853_imad5112_practicum/MainScreen.kt
@@ -1,8 +1,10 @@
 package com.example.a10450853_imad5112_practicum
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.util.LocalePreferences.FirstDayOfWeek.Days
@@ -14,11 +16,10 @@ class MainScreen : AppCompatActivity() {
     private lateinit var minTempEditText: EditText
     private lateinit var maxTempEditText: EditText
     private lateinit var conditionEditText: EditText
-    private var days = ArrayList<String>()
-    private var minTemperature = ArrayList<Int>()
-    private var maxTemperature = ArrayList<Int>()
-    private var weatherCondition = ArrayList<String>()
-
+    private val weekDay = ArrayList<String>()
+    private val minTemperatureList = ArrayList<Int>()
+    private val maxTemperatureList = ArrayList<Int>()
+    private val weatherConditionList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,9 +44,67 @@ class MainScreen : AppCompatActivity() {
             finish()
         }
 
+        addButton.setOnClickListener {
+            addInfo()
+        }
 
+        clearButton.setOnClickListener {
+            clearInformation()
+        }
+
+        detailedViewButton.setOnClickListener {
+            val intent = Intent(this, DetailedViewScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
 
 
 
     }
+
+    private fun addInfo() {
+        val days = dayEditText.text.toString()
+        val minTemperature = minTempEditText.text.toString().toIntOrNull()
+        val maxTemperature = maxTempEditText.text.toString().toIntOrNull()
+        val weatherCondition = conditionEditText.text.toString()
+
+        if (days.isNotEmpty() && minTemperature != null && maxTemperature != null){
+
+            weekDay.add(days)
+            minTemperatureList.add(minTemperature)
+            maxTemperatureList.add(maxTemperature)
+            weatherConditionList.add(weatherCondition)
+
+            dayEditText.text.clear()
+            minTempEditText.text.clear()
+            maxTempEditText.text.clear()
+            conditionEditText.text.clear()
+
+
+            Toast.makeText(this, "Information added", Toast.LENGTH_SHORT).show()
+
+        }
+        else
+        {
+            Toast.makeText(this, "Please enter all information correctly", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun clearInformation(){
+
+        weekDay.clear()
+        minTemperatureList.clear()
+        maxTemperatureList.clear()
+        weatherConditionList.clear()
+
+        dayEditText.text.clear()
+        minTempEditText.text.clear()
+        maxTempEditText.text.clear()
+        conditionEditText.text.clear()
+
+        Toast.makeText(this, "Information successfully cleared", Toast.LENGTH_SHORT).show()
+
+    }
+
 }
