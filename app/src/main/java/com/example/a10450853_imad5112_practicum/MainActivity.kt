3df13c906dc092086276d0402c5dc74cef435a7b
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2024-06-10 08:59:53 +0200

    Created a splash page for The weather app and declared my button variable.

---
+++ app/src/main/java/com/example/a10450853_imad5112_practicum/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.a10450853_imad5112_practicum
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val homePageButton: Button = findViewById(R.id.homePageButton)
+
+
+
+        }
+    }
+}
